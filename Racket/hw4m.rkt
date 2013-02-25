993f0f4985aeb1d8d65c494270108bd18a47fa99
roca <romelcampbell@gmail.com>
2013-02-25 17:01:37 -0500

    Answer to hw4.5

---
+++ Racket/hw4m.rkt
@@ -0,0 +1,99 @@
+
+#lang racket
+
+(provide (all-defined-out)) ;; so we can put tests in a second file
+
+;; put your code below
+
+; basic function
+(define cube1 
+  (lambda (x)
+    (* x (* x x))))
+
+; many functions, such as *, take a variable number of arguments
+(define cube2
+  (lambda (x)
+    (* x x x)))
+
+; syntactic sugar for function definitions
+(define (cube3 x)
+  (* x x x))
+
+
+; Problem 1
+
+(define (sequence_bad_style low high stride)
+  (if (> (+ low stride) high)
+     ( if (> low high) null (list low)   ) 
+     (cons low (sequence (+ low stride) high stride))
+   )
+ )
+
+
+(define (sequence low high stride)
+  ( cond 
+     [ (> (+ low stride) high) null]    
+     [ (> low high) (list low)]
+     [ #t (cons low (sequence (+ low stride) high stride))]
+  )
+  
+  )
+
+; Problem 2
+(define (string-append-map xs suffix)
+        (map 
+          (lambda (x)  
+            (string-append x suffix)  
+          ) 
+          xs
+         )  
+  )
+
+; Problem 3
+(define (list-nth-mod xs n)
+     (cond
+        [ (null? xs)  (error "list-nth-mod: empty list")]
+        [ (< n 0) (error "list-nth-mod: negative number") ]
+        [ #t (last
+              (take 
+               xs 
+               (+ 
+                1 
+                (modulo 
+                  (length xs)
+                  n
+                )
+               ) 
+              )
+             )]
+     )
+ )
+
+
+(define ones (lambda () (cons 1 ones)))
+
+(define (f x) (cons x ( lambda () (f ( + x 1)) )))
+(define nats1 ( lambda() (f 1)))
+
+(define (stream-maker fn arg)
+  (letrec ([f (lambda (x) 
+                (cons x (lambda () (f (fn x arg)))))])
+    (lambda () (f arg))))
+
+(define nats2  (stream-maker + 1))
+(define powers2 (stream-maker * 2))
+
+
+(define (funny_addition x y )
+  (letrec
+      (
+        [sum (+ (abs x) (abs y) )]
+       )
+  ( cond 
+     [ (= (modulo sum 5 ) 0) (* sum -1)]    
+     [ #t sum]
+  )
+  )
+  )
+
+(define funny-number-stream  (stream-maker funny_addition 1))
