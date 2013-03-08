1cba4d5cde10b74c3ac12e00915d06b14a081405
roca <RomelCampbell@gmail.com>
2013-03-08 05:43:06 -0500

    Add tk lib to home machine and made the first Enhancement

--- ruby/hw6assignment.rb
+++ ruby/hw6assignment.rb
@@ -6,6 +6,43 @@
 class MyTetris < Tetris
   # your enhancements here
 
+   def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+
+  def key_bindings
+    @root.bind('n', proc {self.new_game})
+
+    @root.bind('p', proc {self.pause})
+
+    @root.bind('q', proc {exitProgram})
+
+    @root.bind('a', proc {@board.move_left})
+    @root.bind('Left', proc {@board.move_left})
+
+    @root.bind('d', proc {@board.move_right})
+    @root.bind('Right', proc {@board.move_right})
+
+    @root.bind('s', proc {@board.rotate_clockwise})
+    @root.bind('Down', proc {@board.rotate_clockwise})
+
+    @root.bind('w', proc {@board.rotate_counter_clockwise})
+    @root.bind('Up', proc {@board.rotate_counter_clockwise})
+
+    @root.bind('space' , proc {@board.drop_all_the_way})
+
+
+    @root.bind('u', proc {@board.rotate_180})
+ 
+  end
+
+
+
 end
 
 class MyPiece < Piece
@@ -17,5 +54,14 @@ end
 
 class MyBoard < Board
   # your enhancements here
+
+
+  def rotate_180
+    if !game_over? and @game.is_running?
+      @current_block.move(0, 0, 1)
+      @current_block.move(0, 0, 1)
+    end
+    draw
+  end
 
 end
